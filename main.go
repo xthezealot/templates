85f9c9f7c6152e406ab990aa324eb0bbb6e2f4b8
x <x@xxxxxxx.dev>
2024-04-24 01:19:22 +0000

    use go 1.22 net/http method pattern

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ import (
 var staticFiles embed.FS
 
 func main() {
-	http.Handle("/static/", http.FileServer(http.FS(staticFiles)))
+	http.Handle("GET /static/", http.FileServer(http.FS(staticFiles)))
 
-	http.Handle("/", templ.Handler(components.Home()))
+	http.Handle("GET /", templ.Handler(components.Home()))
 
 	log.Println("Listening on " + os.Getenv("ADDR"))
 	log.Panicln(http.ListenAndServe(os.Getenv("ADDR"), http.TimeoutHandler(http.DefaultServeMux, time.Hour, "")))
