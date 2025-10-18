50121a58f8ed84829ddfedc0cd50613097a20f2d
x <x@xxxxxxx.dev>
2025-10-19 00:02:28 +0200

    add fmt

--- index.tsx
+++ index.tsx
@@ -1,17 +1,19 @@
 import { renderToString } from "react-dom/server"
 
 function Page() {
-  return <html lang="en"><body>hello</body></html>
+  return (
+    <html lang="en">
+      <body>hello</body>
+    </html>
+  )
 }
 
 Bun.serve({
   port: 3000,
   routes: {
-    "/": () => {
-      const html = renderToString(<Page />)
-      return new Response(`<!doctype html>${html}`, {
-        headers: {"content-type": "text/html;charset=utf-8"},
-      })
-    },
+    "/": () =>
+      new Response(`<!doctype html>${renderToString(<Page />)}`, {
+        headers: { "content-type": "text/html;charset=utf-8" },
+      }),
   },
 })
